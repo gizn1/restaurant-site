9bdf164adb761f54b48815ae90addc93450d258e
gizn1 <98593507+gizn1@users.noreply.github.com>
2024-06-07 20:14:58 +0300

    Add files via upload

---
+++ js/mail.js
@@ -0,0 +1,10 @@
+
+
+$('.menu_items').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll:1,
+    prevArrow: "<img src='/image/Union (3).svg' class='prev' alt='1 ' style = 'rotate: 180deg;'>",
+    nextArrow: "<img src='/image/Union (3).svg' class='next' alt='1'>",
+
+  });
